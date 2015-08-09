c0d5ba6687c6aa9d2d3d662b98505300aae1a1d8
cappetta <thomas.cappetta@gmail.com>
2015-08-09 01:45:06 -0400

    baseline of puppet-python

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# datastax
+# Overview
+
+This project creates a multi-node cassandra environment w/ a Graphite Reporting Dashboard
+
+
+## useful commands:
+sudo puppet apply --modulepath=./modules/ ./manifests/default.pp
+
+
+### github pages
+Your project page has been created at http://cappetta.github.io/datastax. 
+Read more at https://help.github.com/pages.
--- puppet/manifests/default.pp
+++ puppet/manifests/default.pp
@@ -6,29 +6,62 @@
 #  include devstack
 #}
 
+include stdlib
+#OPTIONS::["system", "pypy", "3", "3.3"]
+# todo: MASTER::DEFECT::install doesn't work.  Analysis Reqired.
+#class { 'python' :
+#  version    => '3',
+#  pip        => true,
+#  dev        => true,
+#  virtualenv => true,
+#  gunicorn   => false,
+#}
 
- class { 'python' :
-    version    => '2.6',
-    pip        => true,
-    dev        => true,
-    virtualenv => true,
-    gunicorn   => false,
+
+file { "/home/vagrant/venv/":
+    ensure => absent,
+    force  => true
+#    notify => File['setup venv'],
   }
 
-file { "/home/vagrant/venv":
-    ensure => "directory"
-  }
+file { "/home/vagrant/venv/":
+  ensure => "directory",
+}
+
+file { "/home/vagrant/venv/python3/":
+ensure => absent,
+force  => true
+}
+
+file { "/home/vagrant/venv/python2.6/":
+ensure => absent,
+force  => true
+}
 
 
-  python::virtualenv { '/home/vagrant/venv' :
-    ensure       => present,
-    version      => '2.6',
-    requirements => '/vagrant/requirements.txt',
-    systempkgs   => true,
-    distribute   => false,
-    venv_dir     => '/home/vagrant/venv/python2.6',
-    owner        => 'vagrant',
-    group        => 'vagrant',
-    cwd          => '/home/vagrant/venv/python2.6',
-    timeout      => 0,
-  }
+python::virtualenv { '/home/vagrant/venv' :
+  ensure       => present,
+  version      => 'system',
+  requirements => '/vagrant/requirements.txt',
+  systempkgs   => true,
+  distribute   => false,
+  venv_dir     => '/home/vagrant/venv/python2.7',
+  owner        => 'vagrant',
+  group        => 'vagrant',
+  cwd          => '/home/vagrant/venv/python2.7',
+  timeout      => 0
+}
+
+#
+#python::virtualenv { '/home/vagrant/venv2' :
+#  ensure       => present,
+#  version      => '3',
+#  requirements => '/vagrant/requirements.txt',
+#  systempkgs   => true,
+#  distribute   => false,
+#  venv_dir     => '/home/vagrant/venv/python3',
+#  owner        => 'vagrant',
+#  group        => 'vagrant',
+#  cwd          => '/home/vagrant/venv/python3',
+#  timeout      => 0,
+#}
