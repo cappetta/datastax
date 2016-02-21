996d091a45a11186f2a8f1294457061603d23d45
cappetta <thomas.cappetta@gmail.com>
2016-02-20 22:38:24 -0500

    minor maintenance

--- setup_openstack_project.sh
+++
@@ -1,8 +0,0 @@
-
-keystone tenant-create --name Cassandra --description "Cassandra Project"
-keystone user-create --name cappetta --pass capp --email thomas.cappetta@gmail.com
-keystone user-role-add --user cappetta --tenant Cassandra --role admin
-
-keystone tenant-create --name MineCraft --description "MineCraft Project"
-keystone user-create --name minecraft --pass m1n3cr4ft_p455w0rd
-keystone user-role-add --user minecraft --project_name MineCraft --role _member_
